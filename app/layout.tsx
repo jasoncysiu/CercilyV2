46212665d15e3886d826db2bf4359d9ac2ced61f
[dyad] <git@dyad.sh>
2026-01-19 22:41:09 +1100

    [dyad] Changed font to IBM Plex Sans - wrote 2 file(s)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import '@/styles/globals.css'
 import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
+import { IBM_Plex_Sans } from 'next/font/google'
 
-const poppins = Poppins({
+const ibmPlexSans = IBM_Plex_Sans({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700'],
-  variable: '--font-poppins',
+  variable: '--font-ibm-plex-sans',
 })
 
 export const metadata: Metadata = {
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`${poppins.variable}`}>
+    <html lang="en" className={`${ibmPlexSans.variable}`}>
       <body>{children}</body>
     </html>
   )
